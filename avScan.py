6c471ea86622d3181c8f394886873a9bafa95bd6
1rm <1315546149@qq.com>
2022-02-16 16:59:26 +0800

    Update avScan.py

--- avScan.py
+++ avScan.py
@@ -1,6 +1,6 @@
 import re
 from argparse import ArgumentParser
-from av_data import av1, av2
+from av_data import av1, av2, av3
 
 def av(tasklist):
     i = 0
@@ -21,6 +21,12 @@ def av(tasklist):
                 i += 1
                 try:
                     print("\033[1;31m[+]\033[1;37m", x, "->", "\033[1;31m" + av2.avList2[x] + "\033[1;37m")
+                except KeyError:
+                    print("\033[1;33m[!]\033[1;37m 进程名字和av指纹名字大小写不一致:" + "\033[1;33m", x, "\033[1;37m")
+            elif x in map(str.lower, av3.avList3.keys()):
+                i += 1
+                try:
+                    print("\033[1;31m[+]\033[1;37m", x, "->", "\033[1;31m" + av3.avList3[x] + "\033[1;37m")
                 except KeyError:
                     print("\033[1;33m[!]\033[1;37m 进程名字和av指纹名字大小写不一致:" + "\033[1;33m", x, "\033[1;37m")
 
